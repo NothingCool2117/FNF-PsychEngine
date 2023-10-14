fc1d5b5851e39668f7938a8f803644b2fd53d2f6
NothingCool2117 <101919735+NothingCool2117@users.noreply.github.com>
2023-10-14 10:05:43 +0700

    Build

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -25,8 +25,8 @@ class Achievements {
 		["Oversinging Much...?",		"Hold down a note for 10 seconds.",					'oversinging',			false],
 		["Hyperactive",					"Finish a Song without going Idle.",				'hype',					false],
 		["Just the Two of Us",			"Finish a Song pressing only two keys.",			'two_keys',				false],
-		["Toaster Gamer",				"Have you tried to run the game on a toaster?",		'toastie',				false],
-		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true]
+		["I'm burning",				"Have you tried to run the game on a toaster?",		'toastie',				false],
+		["Got it",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true]
 	];
 	public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
 
